be74a241de83e3ec1f50b477478cb6921f269946
Никита <nikkozp@hotmail.com>
2020-03-19 00:23:37 +0100

    add missing futures margin type (not in the documentation)

--- Binance.Net/Converters/FuturesMarginTypeConverter.cs
+++ Binance.Net/Converters/FuturesMarginTypeConverter.cs
@@ -13,7 +13,8 @@ namespace Binance.Net.Converters
         protected override List<KeyValuePair<FuturesMarginType, string>> Mapping => new List<KeyValuePair<FuturesMarginType, string>>
         {
             new KeyValuePair<FuturesMarginType, string>(FuturesMarginType.Isolated, "ISOLATED"),
-            new KeyValuePair<FuturesMarginType, string>(FuturesMarginType.Cross, "CROSSED")
+            new KeyValuePair<FuturesMarginType, string>(FuturesMarginType.Cross, "CROSSED"),
+            new KeyValuePair<FuturesMarginType, string>(FuturesMarginType.Cross, "cross") //return on BinanceFuturesStreamPosition
         };
     }
 }
