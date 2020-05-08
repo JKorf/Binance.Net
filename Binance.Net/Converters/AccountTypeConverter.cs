c400aa875cdd5740ce3d8ceadfffb5e4c560d9fc
Никита <nikkozp@hotmail.com>
2020-05-08 15:17:27 +0200

    add LEVERAGED account type

--- Binance.Net/Converters/AccountTypeConverter.cs
+++ Binance.Net/Converters/AccountTypeConverter.cs
@@ -17,6 +17,7 @@ namespace Binance.Net.Converters
             new KeyValuePair<AccountType, string>(AccountType.Spot, "SPOT"),
             new KeyValuePair<AccountType, string>(AccountType.Margin, "MARGIN"),
             new KeyValuePair<AccountType, string>(AccountType.Futures, "FUTURES"),
+            new KeyValuePair<AccountType, string>(AccountType.Leveraged, "LEVERAGED")
         };
     }
 }
