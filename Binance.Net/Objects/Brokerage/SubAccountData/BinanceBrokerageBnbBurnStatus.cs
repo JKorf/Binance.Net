c830de11ac06b7145c145054e4c03ac202593e02
Dmitriy Ugarov <ugar89@gmail.com>
2020-05-08 16:38:49 +0300

    Brokerage client
    Brokerage client

---
+++ Binance.Net/Objects/Brokerage/SubAccountData/BinanceBrokerageBnbBurnStatus.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace Binance.Net.Objects.Brokerage.SubAccountData
+{
+    /// <summary>
+    /// BNB Burn Status
+    /// </summary>
+    public class BinanceBrokerageBnbBurnStatus
+    {
+        /// <summary>
+        /// Sub Account Id
+        /// </summary>
+        public string SubAccountId { get; set; } = "";
+        
+        /// <summary>
+        /// Is Spot BNB Burn
+        /// </summary>
+        [JsonProperty("spotBNBBurn")]
+        public bool IsSpotBnbBurn { get; set; }
+        
+        /// <summary>
+        /// Is Interest BNB Burn
+        /// </summary>
+        [JsonProperty("interestBNBBurn")]
+        public bool IsInterestBnbBurn { get; set; }
+    }
+}
---
+++ Binance.Net/Objects/Brokerage/SubAccountData/BinanceBrokerageTransferResult.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Binance.Net.Objects.Brokerage.SubAccountData
+{
+    /// <summary>
+    /// Transfer Result
+    /// </summary>
+    public class BinanceBrokerageTransferResult
+    {
+        /// <summary>
+        /// Transaction Id
+        /// </summary>
+        [JsonProperty("txnId")]
+        public string Id { get; set; } = "";
+        
+        /// <summary>
+        /// Client Transfer Id
+        /// </summary>
+        [JsonProperty("clientTranId")]
+        public string ClientTransferId { get; set; } = "";
+    }
+}
