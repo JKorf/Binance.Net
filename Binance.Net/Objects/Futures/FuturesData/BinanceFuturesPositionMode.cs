a13d34ee48e369ae11a3ea4b7a99efcf596a264d
Nikita <nikkozp@hotmail.com>
2020-05-31 22:56:47 +0200

    fix GetBrackets add PositionMode

---
+++ Binance.Net/Objects/Futures/FuturesData/BinanceFuturesPositionMode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binance.Net.Objects.Futures.FuturesData
+{
+    /// <summary>
+    /// User's position mode
+    /// </summary>
+    public class BinanceFuturesPositionMode
+    {
+        /// <summary>
+        /// true": Hedge Mode mode; "false": One-way Mode
+        /// </summary>
+        public bool DualSidePosition { get; set; }
+    }
+}
