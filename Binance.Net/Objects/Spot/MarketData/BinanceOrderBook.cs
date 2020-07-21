818a3f3e87b47358406d441e115cbf54f4eb3840
JKorf <jankorf91@gmail.com>
2020-07-21 08:37:41 +0200

    Adjusted order book models

--- Binance.Net/Objects/Spot/MarketData/BinanceOrderBook.cs
+++ Binance.Net/Objects/Spot/MarketData/BinanceOrderBook.cs
@@ -12,7 +12,7 @@ namespace Binance.Net.Objects.Spot.MarketData
     public class BinanceOrderBook : IBinanceOrderBook
     {
         /// <summary>
-        /// The symbol of the order book (only filled from stream updates)
+        /// The symbol of the order book 
         /// </summary>
         [JsonProperty("s")]
         public string Symbol { get; set; } = "";
@@ -22,24 +22,6 @@ namespace Binance.Net.Objects.Spot.MarketData
         /// </summary>
         [JsonProperty("lastUpdateId")]
         public long LastUpdateId { get; set; }
-
-        /// <summary>
-        /// Setter for last update id, need for Json.Net
-        /// </summary>
-        [JsonProperty("u")]
-        public long LastUpdateIdStream { set => LastUpdateId = value; }
-
-        /// <summary>
-        /// Mapping e to prevent collision with capital E
-        /// </summary>
-        [JsonProperty("e")]
-        private string EventType { get; set; } = "";
-
-        /// <summary>
-        /// Event time of the update (stream only)
-        /// </summary>
-        [JsonProperty("E"), JsonConverter(typeof(TimestampConverter))]
-        public DateTime? EventTime { get; set; }
 
         /// <summary>
         /// The id of this update, can be synced with <see cref="BinanceClient.GetOrderBook"/> to update the order book
@@ -53,19 +35,8 @@ namespace Binance.Net.Objects.Spot.MarketData
         public IEnumerable<BinanceOrderBookEntry> Bids { get; set; } = new List<BinanceOrderBookEntry>();
 
         /// <summary>
-        /// Setter for bids (needed forJson.Net)
-        /// </summary>
-        [JsonProperty("b")]
-        public IEnumerable<BinanceOrderBookEntry> BidsStream { set => Bids = value; }
-        /// <summary>
         /// The list of asks
         /// </summary>
         public IEnumerable<BinanceOrderBookEntry> Asks { get; set; } = new List<BinanceOrderBookEntry>();
-
-        /// <summary>
-        /// Setter for asks (needed forJson.Net)
-        /// </summary>
-        [JsonProperty("a")]
-        public IEnumerable<BinanceOrderBookEntry> AsksStream { set => Asks = value; }
     }
 }
