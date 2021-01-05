07447e15eac6b3b3da7dcea18f5ab070ec139809
Nikita <nikkozp@hotmail.com>
2021-01-05 17:51:49 +0100

    add missing type in ExchangeInfo

---
+++ Binance.Net/Enums/UnderlyingType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binance.Net.Enums
+{
+    /// <summary>
+    /// Underlying Type
+    /// </summary>
+    public enum UnderlyingType
+    {
+        /// <summary>
+        /// Coin
+        /// </summary>
+        Coin,
+        /// <summary>
+        /// Index
+        /// </summary>
+        Index
+    }
+}
--- Binance.Net/Objects/Futures/MarketData/BinanceFuturesSymbol.cs
+++ Binance.Net/Objects/Futures/MarketData/BinanceFuturesSymbol.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using System.Collections.Generic;
 using Binance.Net.Converters;
 using Binance.Net.Enums;
+using System;
+using CryptoExchange.Net.Converters;
 
 namespace Binance.Net.Objects.Futures.MarketData
 {
@@ -68,6 +70,36 @@ namespace Binance.Net.Objects.Futures.MarketData
         [JsonProperty("symbol")]
         public string Name { get; set; } = "";
         /// <summary>
+        /// Pair
+        /// </summary>
+        [JsonProperty("pair")]
+        public string Pair { get; set; } = "";
+        /// <summary>
+        /// Delivery Date
+        /// </summary>
+        [JsonProperty("deliveryDate"), JsonConverter(typeof(TimestampConverter))]
+        public DateTime DeliveryDate { get; set; }
+        /// <summary>
+        /// Delivery Date
+        /// </summary>
+        [JsonProperty("onboardDate"), JsonConverter(typeof(TimestampConverter))]
+        public DateTime ListingDate { get; set; }
+        /// <summary>
+        /// Trigger protect
+        /// </summary>
+        public decimal TriggerProtect { get; set; }
+        /// <summary>
+        /// Currently Empty
+        /// </summary>
+        [JsonProperty("underlyingType"), JsonConverter(typeof(UnderlyingTypeConverter))]
+        public UnderlyingType UnderlyingType { get; set; }
+        /// <summary>
+        /// Currently Empty
+        /// </summary>
+        [JsonIgnore]
+        public object[] UnderlyingSupType { get; set; }
+
+        /// <summary>
         /// Allowed order time in force
         /// </summary>
         [JsonProperty(ItemConverterType = typeof(TimeInForceConverter))]
@@ -118,6 +150,12 @@ namespace Binance.Net.Objects.Futures.MarketData
         /// </summary>
         [JsonConverter(typeof(SymbolStatusConverter))]
         public SymbolStatus Status { get; set; }
+
+        /// <summary>
+        /// The status of the symbol
+        /// </summary>
+        [JsonProperty("settlePlan")]
+        public decimal SettlePlan { get; set; }
     }
 
     /// <summary>
@@ -143,10 +181,6 @@ namespace Binance.Net.Objects.Futures.MarketData
         /// </summary>
         [JsonProperty("equalQtyPrecision")]
         public int EqualQuantityPrecision { get; set; }
-
-        /// <summary>
-        /// Trigger protect
-        /// </summary>
-        public decimal TriggerProtect { get; set; }
+       
     }
 }
