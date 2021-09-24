883262c44cd9ed9bb0529a9eb17564a495041a28
Jkorf <jankorf91@gmail.com>
2021-09-24 10:51:50 +0200

    Add bswap endpoints

---
+++ Binance.Net/Converters/LiquidityTypeConverter.cs
@@ -0,0 +1,18 @@
+﻿using Binance.Net.Enums;
+using CryptoExchange.Net.Converters;
+using System.Collections.Generic;
+
+namespace Binance.Net.Converters
+{
+    internal class LiquidityTypeConverter : BaseConverter<LiquidityType>
+    {
+        public LiquidityTypeConverter() : this(true) { }
+        public LiquidityTypeConverter(bool quotes) : base(quotes) { }
+
+        protected override List<KeyValuePair<LiquidityType, string>> Mapping => new List<KeyValuePair<LiquidityType, string>>
+        {
+            new KeyValuePair<LiquidityType, string>(LiquidityType.Single, "SINGLE"),
+            new KeyValuePair<LiquidityType, string>(LiquidityType.Combined, "COMBINATION")
+        };
+    }
+}
