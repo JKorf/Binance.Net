fdcc9236546afd1d6a31654f02b5eea3d227c672
Lukasz Samson <lukaszsamson@gmail.com>
2021-10-13 11:12:05 +0200

    Add userCommissionRate
    https://binance-docs.github.io/apidocs/delivery/en/#user-commission-rate-user_data
    https://binance-docs.github.io/apidocs/futures/en/#user-commission-rate-user_data

--- Binance.Net/SubClients/Futures/Coin/BinanceClientFuturesCoinAccount.cs
+++ Binance.Net/SubClients/Futures/Coin/BinanceClientFuturesCoinAccount.cs
@@ -17,6 +17,7 @@ namespace Binance.Net.SubClients.Futures.Coin
     {
         private const string accountInfoEndpoint = "account";
         private const string futuresAccountBalanceEndpoint = "balance";
+        private const string futuresAccountUserCommissionRateEndpoint = "commissionRate";
 
         /// <summary>
         /// Api path
@@ -74,5 +75,32 @@ namespace Binance.Net.SubClients.Futures.Coin
         }
 
         #endregion
-    }
+
+        #region Future Account User Commission Rate
+
+        /// <summary>
+        /// Gets account commission rates
+        /// </summary>
+        /// <param name="symbol">Symbol</param>
+        /// <param name="receiveWindow">The receive window for which this request is active. When the request takes longer than this to complete the server will reject the request</param>
+        /// <param name="ct">Cancellation token</param>
+        /// <returns>User commission rate information</returns>
+        public async Task<WebCallResult<BinanceFuturesAccountUserCommissionRate>> GetUserCommissionRateAsync(string symbol, long? receiveWindow = null, CancellationToken ct = default)
+        {
+            var timestampResult = await BaseClient.CheckAutoTimestamp(ct).ConfigureAwait(false);
+            if (!timestampResult)
+                return new WebCallResult<BinanceFuturesAccountUserCommissionRate>(timestampResult.ResponseStatusCode, timestampResult.ResponseHeaders, null, timestampResult.Error);
+
+            var parameters = new Dictionary<string, object>
+            {
+                { "symbol", symbol},
+                { "timestamp", BaseClient.GetTimestamp() }
+            };
+            parameters.AddOptionalParameter("recvWindow", receiveWindow?.ToString(CultureInfo.InvariantCulture) ?? BaseClient.DefaultReceiveWindow.TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
+
+            return await BaseClient.SendRequestInternal<BinanceFuturesAccountUserCommissionRate>(FuturesClient.GetUrl(futuresAccountUserCommissionRateEndpoint, Api, Api == "dapi" ? "1" : "2"), HttpMethod.Get, ct, parameters, true).ConfigureAwait(false);
+        }
+
+        #endregion
+  }
 }
