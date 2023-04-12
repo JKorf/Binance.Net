cb2e40dce4faecbb30357fcb8746bc7f47d03a49
SamuelNZ <54571583+HypsyNZ@users.noreply.github.com>
2023-04-13 00:37:06 +1200

    Update SymbolFilterTypeConverter.cs

--- Binance.Net/Converters/SymbolFilterTypeConverter.cs
+++ Binance.Net/Converters/SymbolFilterTypeConverter.cs
@@ -23,6 +23,7 @@ namespace Binance.Net.Converters
             new KeyValuePair<SymbolFilterType, string>(SymbolFilterType.PercentagePriceBySide, "PERCENT_PRICE_BY_SIDE"),
             new KeyValuePair<SymbolFilterType, string>(SymbolFilterType.TrailingDelta, "TRAILING_DELTA"),
             new KeyValuePair<SymbolFilterType, string>(SymbolFilterType.Notional, "NOTIONAL"),
+            new KeyValuePair<SymbolFilterType, string>(SymbolFilterType.IcebergOrders, "EXCHANGE_MAX_NUM_ICEBERG_ORDERS"),
         };
     }
 }
