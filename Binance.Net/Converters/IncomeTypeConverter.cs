85e53a2aa9faf2197e781f817ab63c21a9fafa37
JKorf <jankorf91@gmail.com>
2023-04-17 20:07:46 +0200

    Added new income types

--- Binance.Net/Converters/IncomeTypeConverter.cs
+++ Binance.Net/Converters/IncomeTypeConverter.cs
@@ -17,6 +17,19 @@ namespace Binance.Net.Converters
             new KeyValuePair<IncomeType, string>(IncomeType.FundingFee, "FUNDING_FEE"),
             new KeyValuePair<IncomeType, string>(IncomeType.Commission, "COMMISSION"),
             new KeyValuePair<IncomeType, string>(IncomeType.InsuranceClear, "INSURANCE_CLEAR"),
+            new KeyValuePair<IncomeType, string>(IncomeType.ReferralKickback, "REFERRAL_KICKBACK"),
+            new KeyValuePair<IncomeType, string>(IncomeType.CommissionRebate, "COMMISSION_REBATE"),
+            new KeyValuePair<IncomeType, string>(IncomeType.ApiRebate, "API_REBATE"),
+            new KeyValuePair<IncomeType, string>(IncomeType.ContestReward, "CONTEST_REWARD"),
+            new KeyValuePair<IncomeType, string>(IncomeType.CrossCollateralTransfer, "CROSS_COLLATERAL_TRANSFER"),
+            new KeyValuePair<IncomeType, string>(IncomeType.OptionsPremiumFee, "OPTIONS_PREMIUM_FEE"),
+            new KeyValuePair<IncomeType, string>(IncomeType.OptionsSettleProfit, "OPTIONS_SETTLE_PROFIT"),
+            new KeyValuePair<IncomeType, string>(IncomeType.InternalTransfer, "INTERNAL_TRANSFER"),
+            new KeyValuePair<IncomeType, string>(IncomeType.AutoExchange, "AUTO_EXCHANGE"),
+            new KeyValuePair<IncomeType, string>(IncomeType.DeliveredSettlement, "DELIVERED_SETTELMENT"),
+            new KeyValuePair<IncomeType, string>(IncomeType.CoinSwapDeposit, "COIN_SWAP_DEPOSIT"),
+            new KeyValuePair<IncomeType, string>(IncomeType.CoinSwapWithdraw, "COIN_SWAP_WITHDRAW"),
+            new KeyValuePair<IncomeType, string>(IncomeType.PositionLimitIncreaseFee, "POSITION_LIMIT_INCREASE_FEE"),
         };
     }
 }
--- Binance.Net/Enums/IncomeType.cs
+++ Binance.Net/Enums/IncomeType.cs
@@ -28,6 +28,58 @@
         /// <summary>
         /// Insurance clear
         /// </summary>
-        InsuranceClear
+        InsuranceClear,
+        /// <summary>
+        /// Referral kickback
+        /// </summary>
+        ReferralKickback,
+        /// <summary>
+        /// Commission rebate
+        /// </summary>
+        CommissionRebate,
+        /// <summary>
+        /// Api rebate
+        /// </summary>
+        ApiRebate,
+        /// <summary>
+        /// Contest reward
+        /// </summary>
+        ContestReward,
+        /// <summary>
+        /// Cross collateral transfer
+        /// </summary>
+        CrossCollateralTransfer,
+        /// <summary>
+        /// Options premium fee
+        /// </summary>
+        OptionsPremiumFee,
+        /// <summary>
+        /// Options settle profit
+        /// </summary>
+        OptionsSettleProfit,
+        /// <summary>
+        /// Internal transfer
+        /// </summary>
+        InternalTransfer,
+        /// <summary>
+        /// Auto exchange
+        /// </summary>
+        AutoExchange,
+        /// <summary>
+        /// Delivered settlement
+        /// </summary>
+        DeliveredSettlement,
+        /// <summary>
+        /// Coin swap deposit
+        /// </summary>
+        CoinSwapDeposit,
+        /// <summary>
+        /// Coin swap withdraw
+        /// </summary>
+        CoinSwapWithdraw,
+        /// <summary>
+        /// Position limit increase fee
+        /// </summary>
+        PositionLimitIncreaseFee
     }
 }
