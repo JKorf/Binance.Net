9107a844cce7071eec32721b1c973e75456dda6a
csandr <csandr@inbox.ru>
2023-06-20 12:58:31 +0300

    Revert "Added PayerInfo and ReceiverInfo"
    This reverts commit da97d21ef0d44e480acaf4f4e343bec6f7154461.

--- Binance.Net/Objects/Models/Spot/BinancePayTrade.cs
+++ Binance.Net/Objects/Models/Spot/BinancePayTrade.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using Binance.Net.Enums;
 using CryptoExchange.Net.Converters;
 using Newtonsoft.Json;
@@ -40,16 +41,6 @@ namespace Binance.Net.Objects.Models.Spot
         /// </summary>
         [JsonProperty("fundsDetail")]
         public IEnumerable<BinancePayTradeDetails> Details { get; set; } = Array.Empty<BinancePayTradeDetails>();
-        /// <summary>
-        /// Payer info
-        /// </summary>
-        [JsonProperty("payerInfo")]
-        public BinancePayTradePayerInfo PayerInfo { get; set; } = new BinancePayTradePayerInfo();
-        /// <summary>
-        /// Receiver info
-        /// </summary>
-        [JsonProperty("receiverInfo")]
-        public BinancePayTradeReceiverInfo ReceiverInfo { get; set; } = new BinancePayTradeReceiverInfo();
     }
 
     /// <summary>
@@ -67,79 +58,5 @@ namespace Binance.Net.Objects.Models.Spot
         /// </summary>
         [JsonProperty("amount")]
         public decimal Quantity { get; set; }
-    }
-
-    /// <summary>
-    /// Pay trade payer info
-    /// </summary>
-    public class BinancePayTradePayerInfo
-    {
-        /// <summary>
-        /// Nickname or merchant name
-        /// </summary>
-        [JsonProperty("name")]
-        public string Name { get; set; } = string.Empty;
-        /// <summary>
-        /// Account type，USER for personal，MERCHANT for merchant
-        /// </summary>
-        [JsonProperty("type")]
-        public string Type { get; set; } = string.Empty;
-        /// <summary>
-        /// Binance uid
-        /// </summary>
-        [JsonProperty("binanceId")]
-        public string BinanceId { get; set; } = string.Empty;
-        /// <summary>
-        /// Binance pay id
-        /// </summary>
-        [JsonProperty("accountId")]
-        public string AccountId { get; set; } = string.Empty;
-    }
-
-    /// <summary>
-    /// Pay trade receiver info
-    /// </summary>
-    public class BinancePayTradeReceiverInfo
-    {
-        /// <summary>
-        /// Nickname or merchant name
-        /// </summary>
-        [JsonProperty("name")]
-        public string Name { get; set; } = string.Empty;
-        /// <summary>
-        /// Account type，USER for personal，MERCHANT for merchant
-        /// </summary>
-        [JsonProperty("type")]
-        public string Type { get; set; } = string.Empty;
-        /// <summary>
-        /// Email
-        /// </summary>
-        [JsonProperty("email")]
-        public string Email { get; set; } = string.Empty;
-        /// <summary>
-        /// Binance uid
-        /// </summary>
-        [JsonProperty("binanceId")]
-        public string BinanceId { get; set; } = string.Empty;
-        /// <summary>
-        /// Binance pay id
-        /// </summary>
-        [JsonProperty("accountId")]
-        public string AccountId { get; set; } = string.Empty;
-        /// <summary>
-        /// International area code
-        /// </summary>
-        [JsonProperty("countryCode")]
-        public string CountryCode { get; set; } = string.Empty;
-        /// <summary>
-        /// Phone number
-        /// </summary>
-        [JsonProperty("phoneNumber")]
-        public string PhoneNumber { get; set; } = string.Empty;
-        /// <summary>
-        /// Country code
-        /// </summary>
-        [JsonProperty("mobileCode")]
-        public string MobileCode { get; set; } = string.Empty;
     }
 }
