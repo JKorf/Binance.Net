80e899a1c1f01a70ea5e2df1085145cbc85cba82
JKorf <jankorf91@gmail.com>
2023-10-29 20:05:51 +0100

    wip

---
+++ Binance.Net/Objects/Sockets/BinanceStreamConverter.cs
@@ -0,0 +1,24 @@
+﻿using Binance.Net.Objects.Models.Spot.Socket;
+using CryptoExchange.Net.Converters;
+using CryptoExchange.Net.Objects.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binance.Net.Objects.Sockets
+{
+    public class BinanceStreamConverter : SocketConverter
+    {
+        public override string[] IdFields => new[] { "e" }; 
+
+        public override Type? GetDeserializationType(Dictionary<string, string> idValues, List<MessageListener> listeners)
+        {
+            var eventType = idValues["e"];
+            switch (eventType)
+            {
+                case "trade": return typeof(BinanceStreamTrade);
+                default: return null;
+            }
+        }
+    }
+}
