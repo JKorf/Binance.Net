994ffad90a16cb2e4bb1c5419d1dd2939909778b
JKorf <jankorf91@gmail.com>
2023-12-03 15:46:57 +0100

    Added proper handling for rate limit errors

---
+++ Binance.Net/Objects/BinanceRateLimitError.cs
@@ -0,0 +1,18 @@
+﻿using CryptoExchange.Net.Objects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binance.Net.Objects
+{
+    public class BinanceRateLimitError : ServerRateLimitError
+    {
+        public BinanceRateLimitError(string message) : base(message)
+        {
+        }
+
+        public BinanceRateLimitError(int? code, string message, object? data) : base(code, message, data)
+        {
+        }
+    }
+}
