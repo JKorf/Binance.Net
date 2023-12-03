533d8b48ba4456492b749bd1ae737fae3aa688fb
JKorf <jankorf91@gmail.com>
2023-12-03 15:51:33 +0100

    Comments

--- Binance.Net/Objects/BinanceRateLimitError.cs
+++ Binance.Net/Objects/BinanceRateLimitError.cs
@@ -1,16 +1,26 @@
 ﻿using CryptoExchange.Net.Objects;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Binance.Net.Objects
 {
+    /// <summary>
+    /// Binance rate limit error
+    /// </summary>
     public class BinanceRateLimitError : ServerRateLimitError
     {
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="message"></param>
         public BinanceRateLimitError(string message) : base(message)
         {
         }
 
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="code"></param>
+        /// <param name="message"></param>
+        /// <param name="data"></param>
         public BinanceRateLimitError(int? code, string message, object? data) : base(code, message, data)
         {
         }
