e16f84825112906fa164f7aae07ba9f818676c74
JKorf <jankorf91@gmail.com>
2024-07-19 14:58:26 +0200

    Added integration tests

--- Binance.Net.UnitTests/RestIntergrationTest.cs
+++ Binance.Net.UnitTests/RestIntergrationTest.cs
@@ -1,5 +1,6 @@
 ﻿using CryptoExchange.Net;
 using CryptoExchange.Net.Objects;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,31 +13,61 @@ namespace Binance.Net.UnitTests
 {
     public abstract class RestIntergrationTest<TClient>
     {
-        public abstract TClient GetClient();
+        public abstract TClient GetClient(ILoggerFactory loggerFactory);
         public virtual bool Run { get; set; }
+        public bool Authenticated { get; set; }
 
-        public async Task RunAndCheckResult<T>(Expression<Func<TClient, Task<WebCallResult<T>>>> expre)
+        protected TClient CreateClient()
+        {
+            var fact = new LoggerFactory();
+            fact.AddProvider(new TraceLoggerProvider());
+            return GetClient(fact);
+        }
+
+        protected bool ShouldRun()
         {
             var integrationTests = Environment.GetEnvironmentVariable("INTEGRATION");
             if (!Run && integrationTests != "1")
+                return false;
+
+            return true;
+        }
+
+        public async Task RunAndCheckResult<T>(Expression<Func<TClient, Task<WebCallResult<T>>>> expre, bool authRequest)
+        {
+            if (!ShouldRun())
                 return;
 
-            var client = GetClient();
-            WebCallResult<T> result;
+            var client = CreateClient();
+
             var expressionBody = (MethodCallExpression)expre.Body;
+            if (authRequest && !Authenticated)
+            {
+                Debug.WriteLine($"Skipping {expressionBody.Method.Name}, not authenticated");
+                return;
+            }
+
+            var listener = new EnumValueTraceListener();
+            Trace.Listeners.Add(listener);
+
+            WebCallResult<T> result;
             try
             {
                 result = await expre.Compile().Invoke(client);
             }
             catch (Exception ex)
             {
-                throw new Exception($"Method {expressionBody.Method.DeclaringType.Name}.{expressionBody.Method.Name} threw an exception: " + ex.ToLogString());
+                throw new Exception($"Method {expressionBody.Method.Name} threw an exception: " + ex.ToLogString());
+            }
+            finally
+            {
+                Trace.Listeners.Remove(listener);
             }
 
             if (!result.Success)
-                throw new Exception($"Method {expressionBody.Method.DeclaringType.Name}.{expressionBody.Method.Name} returned error: " + result.Error);
+                throw new Exception($"Method {expressionBody.Method.Name} returned error: " + result.Error);
 
-            Debug.WriteLine($"{expressionBody.Method.DeclaringType.Name}.{expressionBody.Method.Name} {result}");
+            Debug.WriteLine($"{expressionBody.Method.Name} {result}");
         }
     }
 }
