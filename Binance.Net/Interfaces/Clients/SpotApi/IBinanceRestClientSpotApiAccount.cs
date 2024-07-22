9741873f89db59d3f0dd578752e78b10d9057b60
JKorf <jankorf91@gmail.com>
2024-07-22 16:11:59 +0200

    Added SpotApi.Account.GetCommissionRatesAsync

--- Binance.Net/Interfaces/Clients/SpotApi/IBinanceRestClientSpotApiAccount.cs
+++ Binance.Net/Interfaces/Clients/SpotApi/IBinanceRestClientSpotApiAccount.cs
@@ -863,5 +863,15 @@ namespace Binance.Net.Interfaces.Clients.SpotApi
         /// <param name="ct">Cancellation token</param>
         /// <returns></returns>
         Task<WebCallResult<BinanceVipLevelAndStatus>> GetAccountVipLevelAndStatusAsync(int? receiveWindow = null, CancellationToken ct = default);
+
+        /// <summary>
+        /// Get current account commission rates.
+        /// <para><a href="https://binance-docs.github.io/apidocs/spot/en/#query-commission-rates-user_data" /></para>
+        /// </summary>
+        /// <param name="symbol">Symbol</param>
+        /// <param name="receiveWindow">The receive window for which this request is active. When the request takes longer than this to complete the server will reject the request</param>
+        /// <param name="ct">Cancellation token</param>
+        /// <returns></returns>
+        Task<WebCallResult<BinanceCommissions>> GetCommissionRatesAsync(string symbol, int? receiveWindow = null, CancellationToken ct = default);
     }
 }
---
+++ Binance.Net/Objects/Models/Spot/BinanceCommissions.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binance.Net.Objects.Models.Spot
+{
+    /// <summary>
+    /// Binance commissions
+    /// </summary>
+    public record BinanceCommissions
+    {
+        /// <summary>
+        /// Symbol name
+        /// </summary>
+        [JsonPropertyName("symbol")]
+        public string Symbol { get; set; } = string.Empty;
+        /// <summary>
+        /// Standard commission rates on trades from the order.
+        /// </summary>
+        [JsonPropertyName("standardCommission")]
+        public BinanceCommissionInfo StandardCommissions { get; set; } = null!;
+        /// <summary>
+        /// Tax commission rates for trades from the order.
+        /// </summary>
+        [JsonPropertyName("taxCommission")]
+        public BinanceCommissionInfo TaxCommissions { get; set; } = null!;
+        /// <summary>
+        /// Discount commission when paying in BNB
+        /// </summary>
+        [JsonPropertyName("discount")]
+        public BinanceDiscountInfo Discount { get; set; } = null!;
+
+    }
+
+    /// <summary>
+    /// Commission info
+    /// </summary>
+    public record BinanceDiscountInfo
+    {
+        /// <summary>
+        /// Standard commission is reduced by this rate when paying commission in BNB.
+        /// </summary>
+        [JsonPropertyName("discount")]
+        public decimal Discount { get; set; }
+        /// <summary>
+        /// Enabled for account
+        /// </summary>
+        [JsonPropertyName("enabledForAccount")]
+        public bool EnabledForAccount { get; set; }
+        /// <summary>
+        /// Enabled for symbol
+        /// </summary>
+        [JsonPropertyName("enabledForSymbol")]
+        public bool EnabledForSymbol { get; set; }
+        /// <summary>
+        /// Discount asset
+        /// </summary>
+        [JsonPropertyName("discountAsset")]
+        public string DiscountAsset { get; set; } = string.Empty;
+    }
+
+    /// <summary>
+    /// Commission info
+    /// </summary>
+    public record BinanceCommissionInfo
+    {
+        /// <summary>
+        /// Maker fee
+        /// </summary>
+        [JsonPropertyName("maker")]
+        public decimal Maker { get; set; }
+        /// <summary>
+        /// Taker fee
+        /// </summary>
+        [JsonPropertyName("taker")]
+        public decimal Taker { get; set; }
+        /// <summary>
+        /// Buyer fee
+        /// </summary>
+        [JsonPropertyName("buyer")]
+        public decimal Buyer { get; set; }
+        /// <summary>
+        /// Seller fee
+        /// </summary>
+        [JsonPropertyName("seller")]
+        public decimal Sell { get; set; }
+    }
+}
