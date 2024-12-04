f5158e59176442e996a0a486df31fd244bdd9314
Alex1911-Jiang <tjxx_jgd@163.com>
2024-12-04 14:09:32 +0800

    Fix null exceptions caused by invalid key

--- Binance.Net/Clients/GeneralApi/BinanceRestClientGeneralApiCopyTrading.cs
+++ Binance.Net/Clients/GeneralApi/BinanceRestClientGeneralApiCopyTrading.cs
@@ -26,6 +26,12 @@ namespace Binance.Net.Clients.GeneralApi
             var request = _definitions.GetOrCreate(HttpMethod.Get, "sapi/v1/copyTrading/futures/userStatus", BinanceExchange.RateLimiter.SpotRestUid, 20, true);
             var data = await _baseClient.SendAsync<BinanceResult<BinanceCopyTradingUserStatus>>(request, parameters, ct).ConfigureAwait(false);
 
+            if (!data.Success)
+                return data.As<BinanceCopyTradingUserStatus>(default);
+
+            if (data.Data?.Code != 0)
+                return data.AsError<BinanceCopyTradingUserStatus>(new ServerError(data.Data!.Code, data.Data!.Message));
+
             return data.As(data.Data.Data);
         }
 
