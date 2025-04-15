9fd4e0b09dabc043449aadf215e10a5aafd6ca8a
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2025-04-15 21:19:30 +0200

    Added NFT related Interfaces, Models & Enums
    Added:
    - IBinanceRestClientGeneralApiNft interface
    - BinanceNftAsset, BinanceNftDeposit, BinanceNftTransaction, BinanceNftWithdraw, and BinanceListRecords models
    - NftOrderType Enum

---
+++ Binance.Net/Objects/Models/Spot/Nft/BinanceNftDeposit.cs
@@ -0,0 +1,31 @@
+﻿namespace Binance.Net.Objects.Models.Spot.NFT
+{
+    public record BinanceNftDeposit
+    {
+        /// <summary>
+        /// NFT network
+        /// </summary>
+        [JsonPropertyName("network")]
+        public string Network { get; set; } = string.Empty;
+        /// <summary>
+        /// Transaction id
+        /// </summary>
+        [JsonPropertyName("txID")]
+        public string? TxID { get; set; }
+        /// <summary>
+        /// NFT contract address
+        /// </summary>
+        [JsonPropertyName("contractAdrress")] // not a typo, spelled according to binance docs
+        public string ContractAddress { get; set; } = string.Empty;
+        /// <summary>
+        /// NFT token id
+        /// </summary>
+        [JsonPropertyName("tokenId")]
+        public string TokenId { get; set; } = string.Empty;
+        /// <summary>
+        /// Timestamp
+        /// </summary>
+        [JsonPropertyName("timestamp")]
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ Binance.Net/Objects/Models/Spot/Nft/BinanceNftTransaction.cs
@@ -0,0 +1,31 @@
+﻿namespace Binance.Net.Objects.Models.Spot.NFT
+{
+    public record BinanceNftTransaction
+    {
+        /// <summary>
+        /// Order number, 0: purchase order, 1: sell order, 2: royalty income, 3: primary market order, 4: mint fee
+        /// </summary>
+        [JsonPropertyName("orderNo")]
+        public string OrderNo { get; set; } = string.Empty;
+        /// <summary>
+        /// Tokens
+        /// </summary>
+        [JsonPropertyName("tokens")]
+        public IEnumerable<BinanceNftAsset> Tokens { get; set; } = Array.Empty<BinanceNftAsset>();
+        /// <summary>
+        /// Trade time
+        /// </summary>
+        [JsonPropertyName("tradeTime")]
+        public DateTime TradeTime { get; set; }
+        /// <summary>
+        /// Trade amount
+        /// </summary>
+        [JsonPropertyName("tradeAmount")]
+        public string TradeAmount { get; set; } = string.Empty;
+        /// <summary>
+        /// Trade currency
+        /// </summary>
+        [JsonPropertyName("tradeCurrency")]
+        public string TradeCurrency { get; set; } = string.Empty;
+    }
+}
