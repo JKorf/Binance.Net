a0494cc83dfe980f08a81fa99ecf7f3a8d1d6fe9
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2025-05-07 21:30:16 +0200

    Added Enums and Models related to Binance VIP Loan Endpoins
    Added:
    - Enums:
        - VipLoanApplicationStatus
        - VipLoanRepayStatus
    - Models:
        - BinanceVipLoanAccuredInterest
        - BinanceVipLoanApplicationStatus
        - BinanceVipLoanAsset
        - BinanceVipLoanBorrowData
        - BinanceVipLoanBorrowInterestRate
        - BinanceVipLoanCollateralAccountLockedValue
        - BinanceVipLoanCollateralAsset
        - BinanceVipLoanInterestRate
        - BinanceVipLoanOngoingORderData
        - BinanceVipLoanRenewData
        - BinanceVipLoanRepayData
        - BinanceVipLoanRepayHistoryData

---
+++ Binance.Net/Objects/Models/Spot/VipLoans/BinanceVipLoanAccuredInterest.cs
@@ -0,0 +1,41 @@
+﻿using Binance.Net.Enums;
+
+namespace Binance.Net.Objects.Models.Spot.VipLoans
+{
+    /// <summary>
+    /// VIP Loan accured interest
+    /// </summary>
+    public record BinanceVipLoanAccuredInterest
+    {
+        /// <summary>
+        /// Loan asset
+        /// </summary>
+        [JsonPropertyName("loanCoin")]
+        public string LoanAsset { get; set; } = string.Empty;
+        /// <summary>
+        /// Principal quantity
+        /// </summary>
+        [JsonPropertyName("principalAmount")]
+        public decimal PrincipalQuantity { get; set; }
+        /// <summary>
+        /// Interest quantity
+        /// </summary>
+        [JsonPropertyName("interestAmount")]
+        public decimal InterestQuantity { get; set; }
+        /// <summary>
+        /// Annual interest rate
+        /// </summary>
+        [JsonPropertyName("annualInterestRate")]
+        public decimal AnnualInterestRate { get; set; }
+        /// <summary>
+        /// Accrual time
+        /// </summary>
+        [JsonPropertyName("accrualTime")]
+        public DateTime AccrualTime { get; set; }
+        /// <summary>
+        /// Latest order ID for renewal order
+        /// </summary>
+        [JsonPropertyName("orderId")]
+        public string OrderId { get; set; } = string.Empty;
+    }
+}
---
+++ Binance.Net/Objects/Models/Spot/VipLoans/BinanceVipLoanInterestRate.cs
@@ -0,0 +1,24 @@
+﻿namespace Binance.Net.Objects.Models.Spot.VipLoans
+{
+    /// <summary>
+    /// VIP Loan interest rate
+    /// </summary>
+    public record BinanceVipLoanInterestRate
+    {
+        /// <summary>
+        /// Loan asset
+        /// </summary>
+        [JsonPropertyName("coin")]
+        public string LoanAsset { get; set; } = string.Empty;
+        /// <summary>
+        /// Annualized interest rate
+        /// </summary>
+        [JsonPropertyName("annualizedInterestRate")]
+        public decimal AnnualizedInterestRate { get; set; }
+        /// <summary>
+        /// Time
+        /// </summary>
+        [JsonPropertyName("time")]
+        public DateTime Time { get; set; }
+    }
+}
