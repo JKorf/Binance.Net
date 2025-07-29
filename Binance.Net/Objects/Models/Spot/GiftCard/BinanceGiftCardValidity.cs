d587fc8a1b34dd10a26fcad2bd7110ddd66f65f9
Andrii Andreichenko <87246480+ShrewdLuni@users.noreply.github.com>
2025-07-29 16:23:47 +0200

    Added Gift Card Endpoints Support (#1500)
    * Added Gift Card related Interfaces and Models
    
    Added:
    - Interfaces:
      - IBinanceRestClientGeneralApiGiftCard
    - Models:
      - BinanceGiftCardData
      - BinanceGiftCardRedeemData
      - BinanceGiftCardResponse
      - BinanceGiftCardTokenLimit
      - BinanceGiftCardValidity
    
    * Added Gift Card Client Implementation
    
    Implemented Gift Card endpoints

---
+++ Binance.Net/Objects/Models/Spot/GiftCard/BinanceGiftCardValidity.cs
@@ -0,0 +1,24 @@
+﻿namespace Binance.Net.Objects.Models.Spot.GiftCard
+{
+    /// <summary>
+    /// Binance Gift Card validity data
+    /// </summary>
+    public record BinanceGiftCardValidity
+    {
+        /// <summary>
+        /// Is gift card valid
+        /// </summary>
+        [JsonPropertyName("valid")]
+        public bool Valid { get; set; }
+        /// <summary>
+        /// Token
+        /// </summary>
+        [JsonPropertyName("token")]
+        public string Token { get; set; } = string.Empty;
+        /// <summary>
+        /// Amount
+        /// </summary>
+        [JsonPropertyName("amount")]
+        public decimal? Amount { get; set; } = null;
+    }
+}
